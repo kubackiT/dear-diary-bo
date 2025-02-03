f1abd6ba27681534b6c01c956094c5b9105370c2
kubackiT <t.kubacki@interia.pl>
2025-02-03 02:16:44 +0100

    cors fix

--- server.js
+++ server.js
@@ -21,6 +21,18 @@ app.use(
 
 app.options('*', cors(corsOptions));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', authConfig.CORS_ORIGIN);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  next();
+});
+
 // parse requests of content-type - application/json
 app.use(express.json());
 
